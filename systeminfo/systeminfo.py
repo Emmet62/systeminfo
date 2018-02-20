2b8ff1395129facce1d6267f2a5b5786d5e16299
Emmet Tracey <emtracey@tcd.ie>
2018-02-20 21:16:00 +0000

    edited the systeminfo.py file

--- systeminfo/systeminfo.py
+++ systeminfo/systeminfo.py
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+'''
+Created on 20 Feb 2018
+
+@author: Emmet Tracey
+'''
+
+import platform
+
+#syteminfo program
+def get_platform_info():
+	return platform.platform()
+
+print (get_platform_info())
+
+#if __name__ == '__main__':
+#	main()
